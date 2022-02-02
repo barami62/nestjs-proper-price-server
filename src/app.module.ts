cc28dd0a9e3a5a6a0d61eaf99d34c2813e123f0a
barami62 <barami62@yun62.local>
2022-02-02 18:42:27 +0900

    root 경로 부활

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ProperPriceModule } from './proper-price/proper-price.module';
 
 @Module({
   imports: [ProperPriceModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
